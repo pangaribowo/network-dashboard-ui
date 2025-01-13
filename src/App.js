310dd75f2e1212136c807881c05d0eb65dece6ea
pangaribowo <fatahillahalifp04@gmail.com>
2025-01-14 05:37:33 +0700

    chore: initialize basic network dashboard ui project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NetworkDashboard from './components/NetworkDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <NetworkDashboard />
+    </div>
+  );
+}
+
+export default App;
